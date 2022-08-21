bb8caa4a0ab66ac03eb1ea4d8eb970ea7b941edd
Junjie Wu <clatisus@gmail.com>
2022-08-22 01:04:25 +0800

    fix copilot <Tab>

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -27,6 +27,11 @@ return {
 
   ["github/copilot.vim"] = {
     branch = "release",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+    end,
   },
 
   ["vim-scripts/BufOnly.vim"] = {},
