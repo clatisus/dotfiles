d92d8ec424197aa01bf91628d1cca4aeacaf25d3
Junjie Wu <clatisus@gmail.com>
2022-08-28 22:48:30 +0800

    select all by A-a

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -10,6 +10,7 @@ vim.opt.wrap = true
 lvim.leader = "space"
 lvim.keys.normal_mode["<ESC>"] = "<cmd> noh <CR>"
 lvim.keys.normal_mode["<C-c>"] = "<cmd> %y+ <CR>"
+lvim.keys.normal_mode["<A-a>"] = "ggVG"
 lvim.keys.term_mode["<A-x>"] = "<C-\\><C-n>"
 lvim.builtin.which_key.mappings["h"] = nil
 lvim.builtin.which_key.mappings["w"] = nil
