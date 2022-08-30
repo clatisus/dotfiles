3e873cea4363e99871cfa66f740686fbccdcfb81
Junjie Wu <clatisus@gmail.com>
2022-08-30 15:45:55 +0800

    remove <A-l/h>

--- .config/lvim/lua/user/keybindings.lua
+++ .config/lvim/lua/user/keybindings.lua
@@ -4,8 +4,6 @@ M.setup = function()
   lvim.keys.normal_mode["<ESC>"] = "<cmd> noh <CR>"
   lvim.keys.normal_mode["<C-c>"] = "<cmd> %y+ <CR>"
   lvim.keys.normal_mode["<A-a>"] = "ggVG"
-  lvim.keys.normal_mode["<A-l>"] = "<cmd> BufferLineCycleNext <cr>"
-  lvim.keys.normal_mode["<A-h>"] = "<cmd> BufferLineCyclePrev <cr>"
 
   lvim.keys.term_mode["<A-x>"] = "<C-\\><C-n>"
 end
