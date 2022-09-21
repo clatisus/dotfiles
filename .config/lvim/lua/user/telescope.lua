04ac6725810666976622c2c8301446077cbe7e90
Junjie Wu <clatisus@gmail.com>
2022-09-21 23:51:27 +0800

    telescope path_display

--- .config/lvim/lua/user/telescope.lua
+++ .config/lvim/lua/user/telescope.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.setup = function()
   lvim.builtin.telescope.defaults.file_ignore_patterns = { ".git/", "node_modules/" }
+  lvim.builtin.telescope.defaults.path_display = { shorten = 10 }
 end
 
 return M
