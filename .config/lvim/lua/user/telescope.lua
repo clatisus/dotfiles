11ab7a2076c149085255ff6f197fae69ed027a57
Junjie Wu <clatisus@gmail.com>
2022-09-21 23:53:30 +0800

    update path_display

--- .config/lvim/lua/user/telescope.lua
+++ .config/lvim/lua/user/telescope.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
   lvim.builtin.telescope.defaults.file_ignore_patterns = { ".git/", "node_modules/" }
-  lvim.builtin.telescope.defaults.path_display = { shorten = 10 }
+  lvim.builtin.telescope.defaults.path_display = { shorten = { len = 8, exclude = { -1 } } }
 end
 
 return M
