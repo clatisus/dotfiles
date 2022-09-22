3de573f89f7f8332c9cd60b585f815b02af88bf3
Junjie Wu <clatisus@gmail.com>
2022-09-22 11:35:59 +0800

    lualine use progress

--- .config/lvim/lua/user/lualine.lua
+++ .config/lvim/lua/user/lualine.lua
@@ -1,11 +1,7 @@
 local M = {}
 
 M.setup = function()
-  local components = require("lvim.core.lualine.components")
   lvim.builtin.lualine.sections.lualine_a = { "mode" }
-  lvim.builtin.lualine.sections.lualine_z = {
-    components.scrollbar,
-  }
 end
 
 return M
