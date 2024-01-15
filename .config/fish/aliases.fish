5ef1df11ab1185c8f90339081d587969b05100b1
Junjie Wu <clatisus@gmail.com>
2024-01-15 16:59:07 +0800

    add find_and_replace

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -23,6 +23,13 @@ alias rebase="git rebase -i (git merge-base master HEAD)"
 # clean up local branches
 alias gclean="git branch | grep -v '*' | grep -v master | xargs -I {} git branch -D {}"
 
+# find and replace
+function find_and_replace --description "Find and replace a string in all files in the current directory"
+    set search $argv[1]
+    set replace $argv[2]
+    grep -rl "$search" ./ | xargs -I{} sed -i '' "s@""$search""@""$replace""@g" {}
+end
+
 function ll --wraps ls --description "List contents of directory using long format"
     ls -alh $argv
 end
