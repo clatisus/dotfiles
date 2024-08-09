6bd8768a7af70c28030bca1bb061ec1734def553
Junjie Wu <clatisus@gmail.com>
2024-08-09 18:16:22 +0800

    setup neovide & default_path on enter

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,20 @@
 require "nvchad.options"
 
--- add yours here!
+local neovide_config = {
+  neovide_remember_window_size = true,
+  neovide_hide_mouse_when_typing = true,
+  neovide_confirm_quit = true,
+  neovide_input_macos_option_key_is_meta = 'only_left'
+}
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+if vim.g.neovide then
+  for k, v in pairs(neovide_config) do
+    vim.g[k] = v
+  end
+end
+
+-- setup default path
+local default_path = vim.fn.expand("~/")
+if vim.fn.getcwd() == '/' then
+  vim.api.nvim_set_current_dir(default_path)
+end
